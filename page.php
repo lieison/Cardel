e83f25d46f05dc65fa0ba09eb90699de114d4774
Rolando Antonio Arriaza <rolignu90@gmail.com>
2015-05-20 06:23:08 +0000

    CARDEL 90%

--- page.php
+++ page.php
@@ -156,4 +156,108 @@ class Page{
     }
 
     
+    
+   public function FooterContact(){
+        echo ' <footer id="footer" style="background-color:#E5007E;" class="">
+
+			<div class="container">
+
+				<!-- Footer Widgets
+				============================================= -->
+				<div class="footer-widgets-wrap clearfix">
+
+					<div class="col_two_third">
+
+                                            <div class="widget clearfix" style="color:white;">
+
+                                                    <img src="img/logo_white.png" width="200" height="80" alt="" class="alignleft" style="margin-top: 8px; padding-right: 18px;">
+
+							
+
+							<div class="line double-line" style="margin: 60px 0;  "></div>
+                                                        
+                                                        <p style="font-size:200%;"><strong>Direccion:</strong></p>
+                                                        <p style="font-size:large;">
+                                                       
+                                                            FALTA DIRECCION ...
+                                                         
+                                                         </p>
+						</div>
+
+					</div>
+
+					<div class="col_one_third col_last">
+
+						<div class="widget clear-bottommargin-sm clearfix">
+
+							<div class="row">
+
+								<div  class="col-md-12 bottommargin-sm">
+									<div style="color:white;" class="footer-big-contacts">
+										<span style="color:white;">Llámanos:</span>
+										(503) 2122-4434
+									</div>
+								</div>
+
+								<div class="col-md-12 bottommargin-sm">
+									<div style="color:white;" class="footer-big-contacts">
+										<span style="color:white;">Escribenos:</span>
+										info@cardelindustrial.com
+									</div>
+								</div>
+
+							</div>
+
+						</div>
+
+						<div class="widget subscribe-widget clearfix">
+							<div class="row">
+
+								<div class="col-md-8 clearfix bottommargin-sm">
+									<a href="#"  style="margin-right: 10px;">
+                                                                            <img width="50" height="50" src="img/home/logos-social-01.png" />
+									</a>
+                                                                    <a href="#"  style="margin-right: 10px;">
+                                                                            <img width="50" height="50" src="img/home/logos-social-02.png" />
+									</a>
+                                                                    <a href="#"  style="margin-right: 10px;">
+                                                                            <img width="50" height="50" src="img/home/logos-social-03.png" />
+									</a>
+									
+								</div>
+								
+
+							</div>
+						</div>
+
+					</div>
+
+				</div><!-- .footer-widgets-wrap end -->
+
+			</div>
+
+			<!-- Copyrights
+			============================================= -->
+			<div style="background-color:#009EE2" id="copyrights">
+
+				<div class="container clearfix">
+
+					<div class="col_half">
+						Copyrights &copy; 2015 CARDEL, Todos los derechos reservados.<br>
+						<div class="copyright-links"><a href="#">Powered By Lieison Working Together</a> </div>
+					</div>
+
+					<div class="col_half col_last tright">
+						<div class="copyrights-menu copyright-links clearfix">
+							<a href="inidex.php">Inicio</a>/<a href="quienes.php">Quienes somos</a>/<a href="oportunidades.php">Oportunidades</a>/<a href="servicios.php">Servicios</a>/<a href="contacto.php">Contacto</a>
+						</div>
+					</div>
+
+				</div>
+
+			</div><!-- #copyrights end -->
+
+		</footer>';
+    }
+    
 }
